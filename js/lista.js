f7358651bc798e37a9c97f9dfd02eb969e2feefa
Wesley Sampaio Cunha <74968786+WesleySampaioCunha@users.noreply.github.com>
2024-09-16 16:46:46 -0300

    Atualizando o Projeto
    Projeto todo lista Avançados.

---
+++ js/lista.js
@@ -0,0 +1,28 @@
+// Seleção de Elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+
+
+
+// Funções
+const saveTodo = (text, done = 0, save =1) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitile.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+};
+
+
+// Eventos
